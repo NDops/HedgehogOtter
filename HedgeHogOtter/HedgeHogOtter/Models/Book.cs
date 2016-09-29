c2a84f4d19f881b70e93d14e57e9e6f567a6e66c
Nick <Nick@NICK>
2016-09-29 15:33:43 -0500

    Created Book model

---
+++ HedgeHogOtter/HedgeHogOtter/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HedgeHogOtter.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public int ISBN { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string Subject { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+        public string BookCondition { get; set; }
+        public string PublishPlace { get; set; }
+        public int PublishYear { get; set; }
+        public int Quantity { get; set; }
+    }
+}
