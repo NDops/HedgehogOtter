dec51da4c47e14f907f99f6144a8cccd1abf6af6
KronnyEC <kronhoew@uwec.edu>
2016-10-05 16:05:56 -0500

    Web.config for SQL Server. Succesfull connection to Azure Server.

--- HedgeHogOtter/HedgeHogOtter/Controllers/HomeController.cs
+++ HedgeHogOtter/HedgeHogOtter/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,7 +26,41 @@ namespace HedgeHogOtter.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            int a = GetNumberOfRecords();
+
+
             return View();
+
+            
+        }
+
+        public int GetNumberOfRecords()
+        {
+            int count = -1;
+
+            string cs = ConfigurationManager.ConnectionStrings["HHDB"].ConnectionString;
+
+            SqlConnection conn = new SqlConnection(cs);
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // 1. Instantiate a new command
+                SqlCommand cmd = new SqlCommand("select count(*) from dbo.Book", conn);
+
+                // 2. Call ExecuteScalar to send command
+                count = (int)cmd.ExecuteScalar();
+            }
+            finally
+            {
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return count;
         }
     }
 }
