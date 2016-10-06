04b9985962e4ab32a72e1edf5e4a4afc753adc8c
Nick <Nick@NICK>
2016-10-06 17:23:48 -0500

    Three random books appear on home page

--- HedgeHogOtter/HedgeHogOtter/Controllers/HomeController.cs
+++ HedgeHogOtter/HedgeHogOtter/Controllers/HomeController.cs
@@ -16,6 +16,18 @@ namespace HedgeHogOtter.Controllers
 
         public ActionResult Index()
         {
+            var bookList = db.Books.ToList();
+            var bookDisplayList = new List<Book>();
+            while(bookDisplayList.Count < 3)
+            {
+                Random rnd = new Random();
+                int bookIndex = rnd.Next(bookList.Count);
+                if (!bookDisplayList.Contains(bookList.ElementAt(bookIndex)))
+                {
+                    bookDisplayList.Add(bookList.ElementAt(bookIndex));
+                }
+            }
+            ViewBag.bookDisplayList = bookDisplayList;
             return View();
         }
 
@@ -30,41 +42,9 @@ namespace HedgeHogOtter.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
-            int a = GetNumberOfRecords();
-
-
             return View();
 
             
-        }
-
-        public int GetNumberOfRecords()
-        {
-            int count = -1;
-
-            string cs = ConfigurationManager.ConnectionStrings["HHDB"].ConnectionString;
-
-            SqlConnection conn = new SqlConnection(cs);
-            try
-            {
-                // Open the connection
-                conn.Open();
-
-                // 1. Instantiate a new command
-                SqlCommand cmd = new SqlCommand("select count(*) from dbo.Book", conn);
-
-                // 2. Call ExecuteScalar to send command
-                count = (int)cmd.ExecuteScalar();
-            }
-            finally
-            {
-                // Close the connection
-                if (conn != null)
-                {
-                    conn.Close();
-                }
-            }
-            return count;
         }
 
         public ActionResult Login()
